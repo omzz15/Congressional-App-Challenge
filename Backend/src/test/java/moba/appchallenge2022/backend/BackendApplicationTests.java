c260702917e3979c98d315ea38af6be39e7a5022
omzz15 <55567192+omzz15@users.noreply.github.com>
2022-06-20 10:47:52 -0400

    selected backend

---
+++ Backend/src/test/java/moba/appchallenge2022/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package moba.appchallenge2022.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
